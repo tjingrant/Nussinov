7ec6706d3b0da182874bdafb2e4e887d71266008
Jin Tian <tjingrant@gmail.com>
2014-06-10 11:32:50 -0400

    init

---
+++ Main.c
@@ -0,0 +1,16 @@
+/*
+ * Main.c
+ *
+ *  Created on: Jun 9, 2014
+ *      Author: tjin
+ */
+
+#include <stdio.h>
+#define BLOCK_SIZE 4
+
+int main( int argc, const char* argv[] )
+{
+
+	printf( "\nHello World\n\n" );
+}
+
